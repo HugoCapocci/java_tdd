2b9e5d20bbf091ba1a4e948572bfbafd25b55c83
Hugo CAPOCCI <hcapocci@degetel.com>
2018-07-26 20:55:44 +0200

    Initial commit

---
+++ src/test/java/kata/tdd/TestNumberToRomanNumeral.java
@@ -0,0 +1,22 @@
+package kata.tdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import kata.tdd.NumberToRomanNumeral;
+
+public class TestNumberToRomanNumeral {
+
+  NumberToRomanNumeral numberToRomanNumeral;
+
+  @Before
+  public void setUp() {
+    numberToRomanNumeral = new NumberToRomanNumeral();
+  }
+
+  @Test
+  public void testConvert1TtoI() {
+    assertEquals("I", numberToRomanNumeral.toRomanNumber(1));
+  }
+}

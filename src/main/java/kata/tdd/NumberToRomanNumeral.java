7ba2ba2551d81885dbe6b9049bb69bad7240233d
Hugo CAPOCCI <hcapocci@degetel.com>
2018-07-27 10:37:32 +0200

    refactoring with recursivity

--- src/main/java/kata/tdd/NumberToRomanNumeral.java
+++ src/main/java/kata/tdd/NumberToRomanNumeral.java
@@ -5,24 +5,27 @@ import kata.tdd.ConverterRef;
 public class NumberToRomanNumeral {
 
   public static ConverterRef[] refs = {
-    new ConverterRef(1, "I"),
+    new ConverterRef(5, "V"),
     new ConverterRef(4, "IV"),
-    new ConverterRef(5, "V")
+    new ConverterRef(1, "I")
   };
 
   public String toRomanNumber(int numeral) {
+    return toRomanNumber(numeral, 0);
+  }
+
+  public String toRomanNumber(int numeral, int refIndex) {
     String roman = "";
+    ConverterRef ref = refs[refIndex];
 
-    for(ConverterRef ref: refs ) {
-      if(numeral > ref.numeral) {
-        for (int i = 1; i <= numeral; i++) {
-          roman += ref.roman;
-        }
-      } else if (numeral == ref.numeral) {
-        roman = ref.roman;
+    if (numeral == ref.numeral) {
+      return ref.roman;
+    } else if(numeral > ref.numeral) {
+      for (int i = 1; i <= numeral; i++) {
+        roman += ref.roman;
       }
+      return roman;
     }
-
-    return roman;
+    return toRomanNumber(numeral, refIndex + 1);
   }
 }
